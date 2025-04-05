84baee763f5446de59e7ce7926fe19f8fb49dcd6
shashank09mishra <bsmshashankmishra@gmail.com>
2025-04-06 02:03:26 +0530

    add API for production

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,36 @@
+// const express = require('express');
+// const http = require('http');
+// const { Server } = require('socket.io');
+// const cors = require('cors');
+
+// const app = express();
+// app.use(cors());
+// const server = http.createServer(app);
+
+// const io = new Server(server, {
+//   cors: {
+//     origin: 'http://localhost:3000', // React frontend
+//     methods: ['GET', 'POST'],
+//   },
+// });
+
+// io.on('connection', (socket) => {
+//   console.log('User connected');
+
+//   socket.on('chat message', (msg) => {
+//     io.emit('chat message', msg); // Broadcast to all users
+//   });
+
+//   socket.on('disconnect', () => {
+//     console.log('User disconnected');
+//   });
+// });
+
+// server.listen(3001, () => {
+//   console.log('Server listening on port 3001');
+// });
+
+
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
