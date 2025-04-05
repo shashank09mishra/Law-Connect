f39568b812154345e4c075aa55ccb81ca353be29
Shashank Mishra <bsmshashankmishra@gmail.com>
2025-04-06 02:53:16 +0530

    Update ChatBox.js

--- frontend/src/components/ChatBox.js
+++ frontend/src/components/ChatBox.js
@@ -28,6 +28,14 @@ function ChatBox() {
       message: newMessage,
     };
     socket.emit('chat message', msg);
+    // Save message to backen
+    await fetch(${process.env.REACT_APP_API_URL}/api/message, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ message }),
+    });
     setNewMessage('');
   };
 
